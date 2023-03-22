2e8709599c93d4f9214176ba7e22194b27973d94
Nathan Mcfadyen <94457526+GamingGuy4321@users.noreply.github.com>
2023-03-22 14:34:43 -0300

    Finished All Menu's

--- YMJ-The Robbery-Unity/Assets/Scripts/ChangeScene.cs
+++ YMJ-The Robbery-Unity/Assets/Scripts/ChangeScene.cs
@@ -9,4 +9,9 @@ public class ChangeScene : MonoBehaviour
     {
         SceneManager.LoadScene(Game);
     }
+
+    public void ExitGame() {
+        Debug.Log("Exit Button Clicked");
+        Application.Quit();
+    }
 }
